795d86b5238502ea4d576e787f23dd0fd752397f
wanzizhe <1278133518@qq.com>
2017-07-28 09:35:24 +0800

    弹出层（面向对象）的入口文件

---
+++ require.js/弹出层/js/index2.js
@@ -0,0 +1,17 @@
+requirejs.config({
+    paths: {
+        jquery: 'jquery-1.11.2'
+    }
+});
+require(["jquery","second"],function($,Second){
+    $("#btn").on("click",function(){
+        var settings = {
+            width: 500,
+            height: 300,
+            title: "弹出层",
+            content: "login.html"
+        };
+        var dialog = new Second(settings);
+        dialog.open();
+});
+});
