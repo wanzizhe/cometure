4a94edc91b9b5a0fec9fcd214133a84262451e43
wanzizhe <1278133518@qq.com>
2017-11-19 11:21:21 +0800

    vuecli

---
+++ vuecli/src/components/Text.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>{{msg}}</h1>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      msg: 'hhhhhhh'
+    }
+  }
+}
+</script>
+<style>
+h1 {
+  background:#ff0000;
+}
+</style>
