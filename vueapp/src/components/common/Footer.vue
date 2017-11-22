6e1a897c167092b934037aa5fa293a7976b4998d
wanzizhe <1278133518@qq.com>
2017-11-22 18:36:44 +0800

    vue

---
+++ vueapp/src/components/common/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li>电影</li>
+    <li>音乐</li>
+    <li>书籍</li>
+    <li>图片</li>
+  </ul>
+</template><script>
+
+export default {
+
+}
+</script>
+
+ <style scoped>
+
+ </style>
---
+++ vueapp/src/components/common/Header.vue
@@ -0,0 +1,34 @@
+<template>
+      <div class="header">
+        <button class="home">首页</button>
+        <h2 class="title">Movie</h2>
+      </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.header {
+  background-color: rgb(33, 150, 243);
+  width: 100%;
+  height: 1rem;
+  position: fixed;
+  top: 0;
+  line-height: 1rem;
+}
+.home {
+  background: rgb(33, 150, 243);
+  border: 0;
+  color:#fff;
+}
+.title {
+  position: absolute;
+  top:50%;
+  left:50%;
+  transform:translate(-50%,-50%);
+  color:#fff;
+}
+</style>
+
