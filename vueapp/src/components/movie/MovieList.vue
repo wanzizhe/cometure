cd78f024756706194a3aec5d90c799572d809c97
wanzizhe <1278133518@qq.com>
2017-11-25 19:27:19 +0800

    shop

--- vueapp/src/components/movie/MovieList.vue
+++ vueapp/src/components/movie/MovieList.vue
@@ -1,6 +1,6 @@
 <template>
-<div>
-  <ul class="movie-list">
+<div  class="movie-list">
+  <ul>
     <li v-for="movie in movieList" :key="movie.id" class="movie">
       <div class="movie-img">
         <img :src="movie.img" alt="">
@@ -29,17 +29,22 @@ export default {
     }
   },
   mounted(){//钩子函数写在这，请求数据
-    Axios.get(API_PROXY + "http://m.maoyan.com/movie/list.json?type=hot&limit=10&offset="+this.movieList.length)
+      let url1=API_PROXY + "http://m.maoyan.com/movie/list.json?type=hot&limit=10&offset="+this.movieList.length
+    // url1表示猫眼电影的远程服务接口
     //从服务端获取信息所以用get()，向服务端发送信息用post()
     //Axios返回一个promise对象，成功执行then(),失败执行catch()
+    let url2="/static/moviedata.json"//绝对地址，本地的电影数据json文件
+    Axios.get(url2)
     .then(res=>{
       // console.log(res);
       this.loadingShow=false;
-      this.movieList=res.data.data.movies;
+      let list=res.data.data.movies;
+      let data=list.slice(
+        this.movieList.length,this.movieList.length+10
+      )
     })
     .catch(()=>{})
   }
-
 }
 </script>
 
@@ -52,12 +57,14 @@ export default {
 .movie-img{
  flex-grow:1;
  margin-right:0.2rem;
+ width: 0;
 }
 .movie-info{
  flex-grow:2;
+ width: 0;
 }
 .movie-list{
-margin-top:1rem 0;
+ margin:1rem 0;
 }
 .movie-name{
   font-weight:bolder;
