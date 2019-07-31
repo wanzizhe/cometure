fe31c653c2de92dc2e95cb23640b6954b3c3f335
万子哲 <1278133518@qq.com>
2019-07-31 11:04:50 +0800

    琐碎事情

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,21 +1,93 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component,useState }from 'react';
 import './App.css';
+import img from './img/img.jpg'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">第一次学习react</h1>
-        </header>
-        <p className="App-intro">
-          代码在<code>src/App.js</code>文件中修改
-        </p>
+function Test ({ width, height }){
+  const [
+    foldAngle,
+    setFoldAngle,
+  ] = useState(0);
+  
+  //上半部分和下半部分共享的样式
+  const sharedStyles = {
+    width,
+    height: height / 2
+  };
+
+  return (
+    <div style={{ perspective: 500,margin: 200}}>
+
+      {/* 上半部分 */}
+      <div style={{
+          ...sharedStyles,
+          overflow: 'hidden',
+        }}>
+         <img
+          src={img}
+          alt="a neon Chinese alley"
+          style={{
+            width,
+            height,
+          }}
+        />
       </div>
+
+      {/* 下半部分 */}
+      <div
+        style={{
+          ...sharedStyles,
+          backgroundSize: `${width}px ${height}px`,
+          backgroundImage: `url(${img})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: `0px 100%`,
+          transform: `rotateX(${foldAngle}deg)`,
+          transformOrigin: 'center top',
+          // willChange: 'transform',
+          transformStyle: 'preserve-3d',
+        }}
+      >
+        <div style={{
+            position: 'absolute',
+            top: 0, left: 0, right: 0, bottom: 0,
+            background:
+              // 'hsla(0, 100%, 100%, 0.9)',
+              'rgba(255,255,255,0.9)',
+            backfaceVisibility: 'visible',
+            transform:
+              'rotateX(180deg) translateZ(.5px)',
+          }}></div>
+      </div>
+
+      <br />
+      <label htmlFor="slider">Fold ratio:</label>
+      <input
+        id="slider"
+        type="range"
+        min={0}
+        max={180}
+        value={foldAngle}
+        onChange={ev =>
+          setFoldAngle(ev.target.value)
+        }
+        style={{ width }}
+      />
+    </div>
+  );
+}
+
+class Foldable extends Component {
+  constructor( props ) {
+      super( props );
+  }
+
+  componentDidMount(){
+      console.log(this.props.options)
+  }
+  render() {
+    return(
+      <Test {...this.props.options} />
     );
   }
 }
 
-export default App;
+export default Foldable;
--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import FoldableImage from './App';
+import Foldable from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let option = {
+    width:'520',
+    height:'280',
+}
+ReactDOM.render(
+    <Foldable options = {option}/>   
+    , document.getElementById('root')
+);
 registerServiceWorker();
