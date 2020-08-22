d53c02850afefe27a925ff6e213ede1609042226
wanzizhe <1278133518@qq.com>
2020-08-22 08:08:33 +0800

    setState执行顺序实践

--- primary初级/react/my-app/src/App.js
+++ primary初级/react/my-app/src/App.js
@@ -11,33 +11,76 @@ import Spreadsheet from 'x-data-spreadsheet';
 //     }
 //   }
 
-class Example extends React.Component {
-    constructor() {
-      super();
-      this.state = {
-        val: 0
-      };
-    }
+// class Example extends React.Component {
+//     constructor() {
+//       super();
+//       this.state = {
+//         val: 0
+//       };
+//     }
     
+//     componentDidMount() {
+//       this.setState({val: this.state.val + 1});
+//       console.log(this.state.val);    // 第 1 次 log
+  
+//       this.setState({val: this.state.val + 1});
+//       console.log(this.state.val);    // 第 2 次 log
+  
+//       setTimeout(() => {
+//         this.setState({val: this.state.val + 1});
+//         console.log(this.state.val);  // 第 3 次 log
+  
+//         this.setState({val: this.state.val + 1});
+//         console.log(this.state.val);  // 第 4 次 log
+//       }, 0);
+//     }
+  
+//     render() {
+//       return null;
+//     }
+//   };
+// // 答案是0023
+// export default Example;
+
+class Example extends Component {
+    state = {
+      count: 0
+    };
+  
     componentDidMount() {
-      this.setState({val: this.state.val + 1});
-      console.log(this.state.val);    // 第 1 次 log
-  
-      this.setState({val: this.state.val + 1});
-      console.log(this.state.val);    // 第 2 次 log
-  
+      // 生命周期中调用
+      this.setState({ count: this.state.count + 1 });
+      console.log("lifecycle: " + this.state.count);
       setTimeout(() => {
-        this.setState({val: this.state.val + 1});
-        console.log(this.state.val);  // 第 3 次 log
-  
-        this.setState({val: this.state.val + 1});
-        console.log(this.state.val);  // 第 4 次 log
+        // setTimeout中调用
+        this.setState({ count: this.state.count + 1 });
+        console.log("setTimeout: " + this.state.count);
       }, 0);
+      document.getElementById("div2").addEventListener("click", this.increment2);
     }
+  
+    increment = () => {
+      // 合成事件中调用
+      this.setState({ count: this.state.count + 1 });
+      console.log("react event: " + this.state.count);
+    };
+  
+    increment2 = () => {
+      // 原生事件中调用
+      this.setState({ count: this.state.count + 1 });
+      console.log("dom event: " + this.state.count);
+    };
   
     render() {
-      return null;
+      return (
+        <div className="App">
+          <h2>couont: {this.state.count}</h2>
+          <div id="div1" onClick={this.increment}>
+            click me and count+1
+          </div>
+          <div id="div2">click me and count+1</div>
+        </div>
+      );
     }
-  };
-
+  }
 export default Example;
