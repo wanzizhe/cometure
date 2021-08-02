c2786c6eca2843506b26f121c76ac8b13c9d3b5c
wanzizhe <1278133518@qq.com>
2021-08-03 07:42:46 +0800

    feat: lodash的fp模块

--- lagou/10.compose-lodash-fp.js
+++ lagou/10.compose-lodash-fp.js
@@ -1,2 +1,27 @@
-const _ = require('lodash');
+// lodash中的方法和lodash的fp模块对比
 
+// lodash中的方法，变量在前，函数在后（数据优先，函数在后）
+const _ = require('lodash');
+const arr = ['A', 'B', 'C'];
+let arrL = _.map(arr, _.toLower);
+console.log(arrL, 'arrL');
+let arrL2 = _.map(arr);
+console.log(arrL2, 'arrL2');
+
+//lodash的fp模块，里面的函数都是柯里化之后的
+const fp = require('lodash/fp');
+const arrLFP = fp.map(fp.toLower)(arr);
+console.log(arrLFP, 'arrLFP');
+const arrLFP2 = fp.map(fp.toLower, arr);
+console.log(arrLFP2, 'arrLFP2');
+
+//改造之前的函数
+
+const f = fp.flowRight(fp.join('-'), fp.map(_.toLower), fp.split(' '));
+console.log(f('NEVER SAY DIE'), 'NEVER SAY DIE');
+
+// lodash和lodash/fp模块中map方法的区别
+//lodash中的map第二个函数参数接收三个参数（数组每一项，索引，整个数组）
+console.log(_.map(['1', '2', '3'], parseInt))
+//fp中的map第一个函数参数只接受一个参数
+console.log(fp.map(parseInt, ['1', '2', '3']))
--- lagou/9.compose-debugger.js
+++ lagou/9.compose-debugger.js
@@ -2,4 +2,24 @@ const _ = require('lodash');
 
 // 将 NEVER SAY DIE 转化为 never-say-die
 
-const f = 
+//用空格将字符串进行分割
+const split = _.curry((sep, str) => _.split(str, sep));
+
+//转换成小写
+// _.toLower
+
+//用-分隔
+const join = _.curry((sep, arr) => _.join(arr, sep));
+
+//debugger函数
+const debug = _.curry((desc, v) => {
+    console.log(`${desc}------${v}`);
+    return v;
+})
+
+//debug后发现toLower后是个字符串，需要转化成数组
+const map = _.curry((fn, arr)=> _.map(arr, fn));
+
+const f = _.flowRight(join('-'), debug('map返回结果'), map(_.toLower) , debug('split返回结果'), split(' '));
+
+console.log(f('NEVER SAY DIE'));
